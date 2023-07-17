f140f5c0c6026fe3954d2663a9ce359d1b503e5e
chaifeier1029 <139414831+chaifeier1029@users.noreply.github.com>
2023-07-17 09:26:34 +0800

    Update LogisticRegression.py

--- code/model/LogisticRegression.py
+++ code/model/LogisticRegression.py
@@ -1,7 +1,7 @@
 import numpy as np
+import Model
 
-
-class LogisticRegression:
+class LogisticRegression(Model):
     def __init__(self, learning_rate=0.01, num_iterations=1000):
         self.learning_rate = learning_rate
         self.num_iterations = num_iterations
